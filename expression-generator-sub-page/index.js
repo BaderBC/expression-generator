85f2144ca2bceb5a1ac3065fcd14f9719e936100
BaderBC <niejakibader@gmail.com>
2022-05-17 13:52:13 +0200

    origin

--- expression-generator-sub-page/index.js
+++ expression-generator-sub-page/index.js
@@ -1,13 +1,19 @@
+'use strict'
+
 let column = Array(columnLength).fill(''),
     l,
     i;
+
+function onLoad(){
+    tableGenerator().catch(err => console.error(err));
+}
 
 function randomizeStatement(){
     for (let i = 0; i < 6; i++){
         let l = Math.floor(Math.random() * (rowLength - 1) + 1)
         statement(`${l}${i}`)
     }
-    navigator.clipboard.writeText(document.getElementById('korwinsStatement-text').innerText)
+    navigator.clipboard.writeText(document.getElementById('korwinsStatement-text').innerText).then()
 }
 
 function statement(id){
@@ -15,11 +21,34 @@ function statement(id){
     document.getElementById('korwinsStatement-text').innerText =
         column[0] + column[1] + column[2] + column[3] + column[4] + column[5];
     for (i = 0; i < rowLength; i++){
-        console.log(i);
+        //console.log(i);
         l = document.getElementById(`${i}${id.charAt(1)}`)
         l.style.backgroundColor = '#242526'
         l.style.color = '#cccdce'
     }
     document.getElementById(id).style.backgroundColor = '#c8c8e0'
     document.getElementById(id).style.color = '#242526'
+}
+
+
+async function tableGenerator() {
+    let url = 'expression-table.json',
+        fetchTable = fetch(url),
+        doc = document.getElementById('divTableBody');
+
+        await fetchTable
+            .then(res => res.json())
+            .then((data) => {
+                data.forEach((arr, index1) => {
+                    doc.innerHTML += '<div class="divTableRow">'
+
+                    arr.forEach((element, index2) => {
+                        doc.innerHTML += `<div class="divTableCell" id="${index1}${index2}" onclick="statement('${index1}${index2}')">${element}</div>`
+                    })
+
+                    doc.innerHTML += '</div>'
+                })
+            }).catch((err) => {
+                return err;
+            })
 }
